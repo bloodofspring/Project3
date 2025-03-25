4a8745aa558055896e3f55700b3ae74d999a957a
SIlD07 <sychilia07@gmail.com>
2025-03-25 15:20:22 +0300

    the start point

---
+++ main.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.jobs import Jobs
+from data.login_form import LoginForm
+from data.users import User
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+from forms.job_form import JobForm
+
+application = Flask(__name__)
+application.config['SECRET_KEY'] = 'SFgjr35q6#$%n^$n7>z'
+
+login_manager = LoginManager()
+login_manager.init_app(application)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@application.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    users = db_sess.query(User).all()
+    names = {item.id: f"{item.surname} {item.name}" for item in users}
+    return render_template("index.html", jobs=jobs, names=names)
+
+
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@application.route('/add_job', methods=['GET', 'POST'])
+@login_required
+def add_job():
+    form = JobForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        job = Jobs()
+        job.job = form.job.data
+        job.team_leader = form.team_leader.data
+        job.work_size = form.work_size.data
+        job.collaborators = form.collaborators.data
+        job.is_finished = form.is_finished.data
+        db_sess.add(job)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('add_job.html', title='Добавление работы', form=form)
+
+
+@application.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def main():
+    db_session.global_init("db/mars_explorer.db")
+    db_sess = db_session.create_session()
+    application.run()
+
+
+if __name__ == '__main__':
+    main()
